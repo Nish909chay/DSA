8b23b619696fe6801ca3b1aaaba71178fce38033
Nish <nishchaybhosale909@gmail.com>
2025-06-26 21:44:37 +0530

    todays's star pattern done

--- Arrays/starPatterns.py
+++ Arrays/starPatterns.py
@@ -90,7 +90,7 @@ Diamond
  *****
   ***
    *
-"""
+
 n = 4
 for i in range(1,n+1):
     for j in range((n-i)):
@@ -105,6 +105,20 @@ for i in range(n-1,0, -1):
     for k in range((2*i-1),0,-1):
         print("*", end="")
     print()
+"""
 
-   
+"""
+Hollow Square
+*****
+*   *
+*   *
+*****
+"""
+n = 5
+for i in range(1,n):
+    if(i==1 or i==n):
+        print("*" * n)
+    else:
+        print("*" + " " * (n-2) + "*")
+    
 
