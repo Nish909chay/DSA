344f97bdb51c443c54e72c8668d9b1fe712c1bf3
Nish <nishchaybhosale909@gmail.com>
2025-07-06 06:29:58 +0530

    stack and queue

--- Arrays/hashMap.py
+++ Arrays/hashMap.py
@@ -658,3 +658,49 @@ print(lst)   # Convert back to list if needed
 longest = max(lst, key=len)
 print(longest)
 """
+"""
+155. Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(self.min_stack[-1], val))
+            
+    def pop(self):
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        return self.min_stack[-1]
+        
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+"""
