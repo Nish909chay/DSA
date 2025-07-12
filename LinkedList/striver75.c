3134a624ad621750945a37bd99707cdb6f481206
Nish <nishchaybhosale909@gmail.com>
2025-07-13 04:11:55 +0530

    done with day 17

--- LinkedList/striver75.c
+++ LinkedList/striver75.c
@@ -25,7 +25,7 @@ Easy
 Given head, the head of a linked list, determine if the linked list has a cycle in it.
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
 Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
-*/
+---------------------------------------------------------------------------------------------------------------------------------------------
 
 bool hasCycle(Node *head)
 {
@@ -45,3 +45,52 @@ bool hasCycle(Node *head)
     }
     return 0;
 }
+
+
+21. Merge Two Sorted Lists
+Easy
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+------------------------------------------------------------------------------------------------------------------------
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) 
+{
+        if(list1 == NULL && list2 == NULL)
+            return NULL;
+        if(list1 == NULL)
+            return list2;
+        if(list2 == NULL)
+            return list1;
+
+        struct ListNode *curr = list1;
+        while(curr->next != NULL)
+        {
+            curr = curr->next;
+        }
+        curr->next = list2;
+
+        return list1;
+}
+---------------------------------------------------------------------------------------------
+
+21. Merge Two Sorted Lists
+Node* merge(Node* list1,Node* list2) 
+{
+    if(list1 == NULL)
+        return list2;
+    else if(list2 == NULL)
+        return list1;
+
+    if(list1->val <= list2->val)
+    {
+        list1->next = merge(list1->next, list2);
+        return list1;
+    }
+    else if(list1->val >= list2->val)
+    {
+        list2->next = merge(list1, list2->next);
+        return list2;
+    }
+}
+*/
