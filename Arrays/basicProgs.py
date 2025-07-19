1d4c8bec3bc71fae28c842206e730763748c9e8e
Nish <nishchaybhosale909@gmail.com>
2025-07-20 04:04:05 +0530

    then did Palindromic Partitioning

--- Arrays/basicProgs.py
+++ Arrays/basicProgs.py
@@ -445,8 +445,7 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 Example 1:
 Input: numRows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
-"""
-
+-------------------------------------------------
 n = 5
 arr = [[1], [1,1]]
 
@@ -460,8 +459,8 @@ for i in range(2, n):
             this_row.append(prev_row[j-1] + prev_row[j])
     arr.append(this_row)
 
-
 print(arr)
+"""
 
 
 
