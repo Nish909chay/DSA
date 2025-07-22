cc03ed6d50563241b41cd1f3ae7a5cef1f6b3021
Nish <nishchaybhosale909@gmail.com>
2025-07-23 02:19:37 +0530

    caffeine break

--- Arrays/hashMap.py
+++ Arrays/hashMap.py
@@ -557,7 +557,7 @@ Example 1:
 
 Input: nums = [2,5,6,0,0,1,2], target = 0
 Output: true
-"""
+
 arr = [2,5,6,0,0,1,2]
 target = 0
 low = 0
@@ -588,6 +588,68 @@ if found:
     print("found")
 else:
     print("not here")
+"""
+
+"""
+34. Find First and Last Position of Element in Sorted Array
+Medium
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+----------------------not allowed as O(log n) required -----------------this is O(n)-----------------------------------------------
+arr = [5,7,7,8,8,10]
+target = 8
+
+# store position of each element in dict
+from collections import defaultdict
+dict = defaultdict(list)
+for i in range(len(arr)):
+    dict[arr[i]].append(i)
+
+print(dict[target])
+"""
+arr = [5,7,7,8,8,10]
+target = 8
+left = 0
+right = len(arr) - 1
+
+# to find leftmost
+l = -1
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+        l = mid
+        right = mid - 1
+    elif arr[mid] > target:
+        right = mid - 1
+    else:
+        left = mid + 1
+
+# to find rightmost
+left = 0
+right = len(arr) - 1
+r = -1
+while left <= right:
+    mid = (left + right) // 2
+    if arr[mid] == target:
+        r = mid
+        left = mid + 1
+    elif arr[mid] > target:
+        right = mid - 1
+    else:
+        left = mid + 1
+
+res = [l,r]
+
+print(res)
+    
+    
+
+
 
         
 
